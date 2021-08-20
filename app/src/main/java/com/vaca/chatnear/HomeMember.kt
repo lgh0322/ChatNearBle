040e66e2911a1830b7d7e20127282ae3c64cef62
李光浩 <liguanghao@viatomtech.com>
2021-08-20 18:12:09 +0800

    vbn55445

---
+++ app/src/main/java/com/vaca/chatnear/HomeMember.kt
@@ -0,0 +1,3 @@
+package com.vaca.chatnear
+
+data class HomeMember(var name: String, var select: Boolean)
--- app/src/main/java/com/vaca/chatnear/MainActivity.kt
+++ app/src/main/java/com/vaca/chatnear/MainActivity.kt
@@ -2,10 +2,45 @@ package com.vaca.chatnear
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.vaca.chatnear.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+
+
+
+
+        val buttonAdapter = RecordButtonAdapter(requireContext())
+        val lm = object : LinearLayoutManager(requireContext(), HORIZONTAL, false) {
+            override fun canScrollHorizontally(): Boolean {
+                return false
+            }
+        }
+        binding.topButton.layoutManager = lm
+        buttonAdapter.addAll(arrayOf("心电记录", "血压、血氧记录", "体温记录"))
+        buttonAdapter.myGo = object : RecordButtonAdapter.WantInfo {
+            override fun go(u: Int) {
+                if (currentIndex == u) {
+                    return
+                }
+                currentIndex = u
+                if (navController.popBackStack(topId[u], false)) {
+
+                } else {
+                    navController.navigate(topId[u])
+                }
+
+            }
+
+        }
+        binding.topButton.adapter = buttonAdapter
     }
 }
