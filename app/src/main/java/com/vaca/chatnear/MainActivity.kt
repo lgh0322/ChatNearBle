630881bc3eb92f19cfd9dd9e89e4ffc05c92a819
李光浩 <liguanghao@viatomtech.com>
2021-08-20 17:47:14 +0800

    Initial commit

---
+++ app/src/main/java/com/vaca/chatnear/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.vaca.chatnear
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
